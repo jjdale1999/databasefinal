ccbda4cdbe205a6f1902281e5a026dfb7d2bdfec
jjdale1999 <dalejordanne@gmail.com>
2020-04-18 11:03:57 -0500

    friendlist partially styled

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,25 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField,TextAreaField,SelectField
+from wtforms import StringField,TextAreaField,SelectField,PasswordField
 from wtforms.validators import DataRequired, Email
 
 from wtforms import PasswordField
 from wtforms.validators import InputRequired
-class CreateProfile(FlaskForm):
+class SignUp(FlaskForm):
     fname = StringField('First Name', validators=[DataRequired()],render_kw={"placeholder":"Jane"})
     lname = StringField('Last Name', validators=[DataRequired()],render_kw={"placeholder":"Doe"})
     email = StringField('Email', validators=[DataRequired(), Email()],render_kw={"placeholder":"eg. jdoe@example.com"})
+    gender = SelectField('Gender',choices=[('Male','Male'),('Female','Female')])
+    password = PasswordField('Password',validators=[DataRequired()])
+    repassword = PasswordField('Re-Type Password',validators=[DataRequired()])
+
+
+class CreateProfile(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()],render_kw={"placeholder":"Jane"})
     location = StringField('Location', validators=[DataRequired()],render_kw={"placeholder":"eg. Kingston,Jamaica"})
     gender = SelectField('Gender',choices=[('Male','Male'),('Female','Female')])
     biography = TextAreaField('Biography',validators=[DataRequired()])
-    photo = FileField('Photo',validators=[FileRequired(),FileAllowed(['jpg','jpeg','png','Images Only'])])
+    profilepic = FileField('Profile Pic',validators=[FileRequired(),FileAllowed(['jpg','jpeg','png','Images Only'])])
 
 
 
