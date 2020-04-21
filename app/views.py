1dd908013c89ae751290f9670ede82cac9698065
jjdale1999 <dalejordanne@gmail.com>
2020-04-21 00:10:54 -0500

    friend list working

--- app/views.py
+++ app/views.py
@@ -53,7 +53,7 @@ def signup():
 
 @app.route('/friendlist/<userid>',methods=['GET'])
 def friendlist(userid):
-    friends=db.engine.execute("select * from friendship join users on users.userid=friendship.fuserid where friendship.userid='"+userid+"'")  
+    friends=db.engine.execute("select * from friendship join users on users.userid=friendship.fuserid join profiles on profiles.userid=users.userid where friendship.userid='"+userid+"'")  
     profile=db.engine.execute("select * from profiles join users on profiles.userid=users.userid where users.userid='"+userid+"'")  
     followings=db.engine.execute("select count(fuserid) as following from friendship where userid='"+userid+"'")   
     follower=db.engine.execute("select count(userid) as following from friendship where fuserid='"+userid+"'")  
@@ -69,8 +69,9 @@ def friendlist(userid):
         username=y.username
         location=y.countryliving
         biography=y.biography
+        profilepic=y.profilepic
    
-    return render_template('friendslist.html',friends=friends,fname=fname,username=username,lname=lname,email=email,location=location,biography=biography,followers=followers,following=following)
+    return render_template('friendslist.html',friends=friends,profilepic=profilepic,fname=fname,username=username,lname=lname,email=email,location=location,biography=biography,followers=followers,following=following)
 @app.route('/setupprofile',methods=['POST', 'GET'])
 def setupprofile():
     createprofile = CreateProfile()
